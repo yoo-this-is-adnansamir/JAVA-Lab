1e34319747d8bd6162d0686a186d29c758f07ff1
yoo-this-is-adnansamir <adnansamir73937@gmail.com>
2026-06-25 22:21:03 +0600

    Create Main2.java

---
+++ Lab-05/Main2.java
@@ -0,0 +1,30 @@
+class BankAccount {
+int accountNumber;
+String accountHolderName;
+double balance;
+static String bankName = "Pubali Bank";
+BankAccount(int accNum, String name, double bal) {
+accountNumber = accNum;
+accountHolderName = name;
+balance = bal;
+}
+void displayAccountInfo() {
+System.out.println("Bank Name: " + bankName);
+System.out.println("Account Number: " + accountNumber);
+System.out.println("Account Holder: " + accountHolderName);
+System.out.println("Balance: " + balance);
+}
+static void showBankName() {
+System.out.println("Bank Name: " + bankName);
+}
+}
+
+public class Main2 {
+public static void main(String[] args) {
+BankAccount account1 = new BankAccount(101, "Adnan", 20000);
+BankAccount account2 = new BankAccount(102, "Ahnaf", 60000);
+account1.displayAccountInfo();
+System.out.println();
+account2.displayAccountInfo();
+BankAccount.showBankName();
+}
