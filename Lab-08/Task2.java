289fd53e6c531273a31d30af99059ea6f5370172
yoo-this-is-adnansamir <adnansamir73937@gmail.com>
2026-06-25 22:43:35 +0600

    Create Task2.java

---
+++ Lab-08/Task2.java
@@ -0,0 +1,32 @@
+class MyShape { 
+    void draw() { 
+        System.out.println("Drawing shape"); 
+    } 
+} 
+ 
+class Circle extends MyShape { 
+    @Override 
+    void draw() { 
+        System.out.println("Drawing Circle"); 
+    } 
+} 
+ 
+class Rectangle extends MyShape { 
+    @Override 
+    void draw() { 
+        System.out.println("Drawing Rectangle"); 
+    } 
+} 
+ 
+public class MyShapeStudy { 
+    public static void main(String[] args) { 
+ 
+        MyShape s = new MyShape(); 
+        Circle c = new Circle(); 
+        Rectangle r = new Rectangle(); 
+ 
+        s.draw(); 
+        c.draw(); 
+        r.draw(); 
+    } 
+}
